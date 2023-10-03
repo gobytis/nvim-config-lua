a7e89095f73bcafc1ba3dc8dd30ae315dad5042d
YOKOTA Kohei <yokota.kohei@intra.chino-js.com>
2023-10-03 19:02:39 +0900

    indent_blankline の更新に伴って、設定を書き換えました。

--- lua/indent_blankline_config.lua
+++ lua/indent_blankline_config.lua
@@ -1,7 +1,1 @@
-require("indent_blankline").setup {
-    -- for example, context is off by default, use this to turn it on
-    -- NOTE: 下記 2 つはnvim-treesitter がないとエラーになるので現在は停止中です。
-    -- show_current_context = true,
-    -- show_current_context_start = true,
-    show_end_of_line = true
-}
+require("ibl").setup()
